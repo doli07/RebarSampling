3891733af912db49382f41a7926674f2dba86dc0
doli07 <279936012@qq.com>
2023-06-27 16:52:52 +0800

    增加工单数据结构的原材长度

--- RebarSampling/General/GeneralWorkBill.cs
+++ RebarSampling/General/GeneralWorkBill.cs
@@ -90,6 +90,10 @@ namespace RebarSampling
         /// 直径
         /// </summary>
         public int Diameter { get; set; }
+        /// <summary>
+        /// 钢筋原材长度，9米或12米
+        /// </summary>
+        public int OriginalLength { get; set; }
 
         public List<SingleRebarData> RebarList { get; set; }
     }
